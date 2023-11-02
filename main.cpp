dee8eac485e92d71dbe52f55261f8363323d19eb
svzag <svzagarov@gmail.com>
2023-11-02 22:34:18 +0600

    add class Menu

--- main.cpp
+++ main.cpp
@@ -62,19 +62,47 @@ public:
 #endif
     }
 
-    Screen (const Screen &screen)
-            = default;
+    Screen(const Screen &screen)
+    = default;
 
     void printSize() const {
         cout << "x = " << size_x << ",   y = " << size_y << endl;
     }
 };
 
+
+class Menu {
+public:
+    Menu() = default;
+
+    static void showMenu() {
+        cout << CLEAR;
+        cout << MAGENTA << "Welcom to " << CYAN << "LionFeeding" << RESET << endl;
+        cout << "For selected input number on menu and press to enter" << endl;
+        cout << "1. " << CYAN << "Start Game vs PC" << RESET << endl;
+        cout << "2. " << CYAN << "Start Game 1 vs 1" << RESET << endl;
+        cout << "3. " << GREEN << "Exit" << RESET << endl;
+    }
+
+    static void showDifficulty() {
+        cout << CLEAR;
+        cout << "Please option difficulty" << endl;
+        cout << "1. " << GREEN << "Low" << RESET << endl;
+        cout << "2. " << BLUE << "Medium" << RESET << endl;
+        cout << "3. " << RED << "Hard" << RESET << endl;
+        cout << "4. Back to Menu" << endl;
+    }
+
+    static void showRules() {
+        cout << CLEAR;
+        cout << "You can moving with the help keypress w,a,d,s and for 2 player with help i,j,k,l" << endl;
+        cout << "You target collect as much as possible eats until the time is up" << endl;
+        cout << "If you undestend rules, then keypress enter and start games" << endl;
+    }
+};
+
+
 int main() {
     Screen screen;
     screen.printSize();
 }
-
-
-
-

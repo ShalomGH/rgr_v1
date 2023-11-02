a6636cb6ea2bcb4ef5dd3e2c9cf89bf08ce34240
ShalomGH <ShalomGH@mail.ru>
2023-11-02 20:57:39 +0600

    add compatibility

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+#include <iostream>
+
+#ifdef _WIN32
+
+#include <windows.h>
+
+#else
 #include <sys/ioctl.h>
-#include "iostream"
 #include <unistd.h>
+#endif
 
 using namespace std;
 
@@ -10,10 +17,17 @@ public:
     int size_y;
 
     Screen() {
+#ifdef _WIN32
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+        size_x = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        size_y = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+#else
         struct winsize size{};
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
         size_y = size.ws_row;
         size_x = size.ws_col;
+#endif
     }
 
     void printSize() const {

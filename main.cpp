1a1ba155dffdac38aec8e7ecf57b9f68f0adfe4f
ShalomGH <ShalomGH@mail.ru>
2023-11-03 03:11:24 +0600

    add menu, add render

--- main.cpp
+++ main.cpp
@@ -1,29 +1,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <conio.h>
-
-//#include <string>
-//#include <random>
-//#include <unistd.h>
-//#include <ctime>
-//#include <fcntl.h>
-//#include <algorithm>
-//#include <map>
-//#include <cmath>
-//#include <tuple>
-//#include <thread>
-//#include <chrono>
 
 
-#define RESET   "\033[0m"
+//#define RESET   "\033[0m"
 //#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
+//#define RED     "\033[31m"      /* Red */
+//#define GREEN   "\033[32m"      /* Green */
 //#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
+//#define BLUE    "\033[34m"      /* Blue */
+//#define MAGENTA "\033[35m"      /* Magenta */
+//#define CYAN    "\033[36m"      /* Cyan */
 //#define WHITE   "\033[37m"      /* White */
 //#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
 //#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
@@ -39,10 +26,26 @@
 #ifdef _WIN32
 
 #include <windows.h>
+#include <conio.h>
 
 #else
+
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <termios.h>
+
+int getch() {
+    struct termios oldt{}, newt{};
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+
 #endif
 
 using namespace std;
@@ -52,35 +55,34 @@ static int SCREEN_WIDTH;
 
 class Screen {
 public:
-    int size_x;
-    int size_y;
+    vector<vector<char>> canvas;
 
     Screen() {
 #ifdef _WIN32
         CONSOLE_SCREEN_BUFFER_INFO csbi;
         GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-        size_x = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-        size_y = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+        SCREEN_WIDTH = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        SCREEN_HEIGHT = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 #else
         struct winsize size{};
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-        size_y = size.ws_row;
-        size_x = size.ws_col;
+        SCREEN_HEIGHT = size.ws_row;
+        SCREEN_WIDTH = size.ws_col;
 #endif
-        SCREEN_WIDTH = size_x;
-        SCREEN_HEIGHT = size_y;
+        canvas.resize(SCREEN_HEIGHT);
+        for (auto &i: canvas) i.resize(SCREEN_WIDTH);
     }
 
     Screen(const Screen &screen)
     = default;
 
     void printSize() const {
-        cout << "x = " << size_x << ",   y = " << size_y << endl;
+        cout << "x = " << SCREEN_WIDTH << ",   y = " << SCREEN_HEIGHT << endl;
     }
 
-    void Write(vector<vector<char>> scene) const {
-        for(int i = 0; i < SCREEN_HEIGHT; ++i) {
-            for(int j = 0; j < SCREEN_WIDTH; ++j)
+    static void Write(vector<vector<char>> scene) {
+        for (int i = 0; i < SCREEN_HEIGHT; ++i) {
+            for (int j = 0; j < SCREEN_WIDTH; ++j)
                 if (scene[i][j])
                     cout << scene[i][j];
                 else
@@ -92,50 +94,67 @@ public:
 
 
 class Menu {
+private:
+    vector<string> menu_items{
+            "Menu",
+            "1.  Graphic  ",
+            "3.  Table    ",
+            "4.  Animation",
+            "5.  Hui      ",
+    };
 public:
     Menu() = default;
 
-    static vector<vector<char>> showMenu() {
+    vector<vector<char>> render() {
         vector<vector<char>> canvas;
         canvas.resize(SCREEN_HEIGHT);
-        for(auto& i:canvas) i.resize(SCREEN_WIDTH);
+        for (auto &i: canvas) i.resize(SCREEN_WIDTH);
 
-        cout << CLEAR;
-        cout << MAGENTA << "Welcome to " << CYAN << "LionFeeding" << RESET << endl;
-        cout << "For selected input number on menu and press to enter" << endl;
-        cout << "1. " << CYAN << "Start Game vs PC" << RESET << endl;
-        cout << "2. " << CYAN << "Start Game 1 vs 1" << RESET << endl;
-        cout << "3. " << GREEN << "Exit" << RESET << endl;
+        size_t y_start = (SCREEN_HEIGHT - menu_items.size()) / 2;
+
+        for (int i = 0; i < menu_items.size(); i++) {
+            size_t x_start = (SCREEN_WIDTH - menu_items[i].size()) / 2;
+            for (int j = 0; j < menu_items[i].size(); j++) {
+                canvas[i + y_start][j + x_start] = menu_items[i][j];
+            }
+        }
+
+//        cout << CLEAR;
+//        cout << MAGENTA << "Welcome to " << CYAN << "LionFeeding" << RESET << endl;
+//        cout << "For selected input number on menu and press to enter" << endl;
+//        cout << "1. " << CYAN << "Start Game vs PC" << RESET << endl;
+//        cout << "2. " << CYAN << "Start Game 1 vs 1" << RESET << endl;
+//        cout << "3. " << GREEN << "Exit" << RESET << endl;
         return canvas;
-    }
-
-    static void showDifficulty() {
-        cout << CLEAR;
-        cout << "Please option difficulty" << endl;
-        cout << "1. " << GREEN << "Low" << RESET << endl;
-        cout << "2. " << BLUE << "Medium" << RESET << endl;
-        cout << "3. " << RED << "Hard" << RESET << endl;
-        cout << "4. Back to Menu" << endl;
-    }
-
-    static void showRules() {
-        cout << CLEAR;
-        cout << "You can moving with the help keypress w,a,d,s and for 2 player with help i,j,k,l" << endl;
-        cout << "You target collect as much as possible eats until the time is up" << endl;
-        cout << "If you undestend rules, then keypress enter and start games" << endl;
     }
 };
 
 
 int main() {
     Screen screen;
-    vector<vector<char>> canvas;
-    canvas.resize(SCREEN_HEIGHT);
-    for (auto &i: canvas) i.resize(SCREEN_WIDTH);
-    for (auto &i:canvas)
-        for (auto &j:i)
-            j = '0';
-    screen.Write(canvas);
-    screen.printSize();
-    getch();
+    Menu menu;
+    while (true) {
+        Screen::Write(menu.render());
+        int input = getch();
+        switch (input) {
+            case 49: // 1
+                cout << 1 << endl;
+                break;
+            case 50: // 2
+                cout << 2 << endl;
+                break;
+            case 51: // 3
+                cout << 3 << endl;
+                break;
+            case 52: // 4
+                cout << 4 << endl;
+                break;
+            case 53: // 5
+                cout << 5 << endl;
+                break;
+            case 27: // esc
+                cout << CLEAR;
+                exit(1);
+        }
+    }
 }

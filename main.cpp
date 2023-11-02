b9f143f277b32ef066196d3bfb7ccdacc820e709
starArsonist <sternarsonist@gmail.com>
2023-11-02 22:49:05 +0600

    added copy Screen constructor

--- main.cpp
+++ main.cpp
@@ -83,6 +83,8 @@ public:
 
     static vector<vector<char>> showMenu() {
         vector<vector<char>> canvas;
+        canvas.resize(SCREEN_HEIGHT);
+        for(auto i:canvas) i.resize(SCREEN_WIDTH);
 
         cout << CLEAR;
         cout << MAGENTA << "Welcome to " << CYAN << "LionFeeding" << RESET << endl;

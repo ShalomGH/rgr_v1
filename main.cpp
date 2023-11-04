2c91784d29d8996737302eb7f23163a1d3219e84
starArsonist <sternarsonist@gmail.com>
2023-11-04 11:49:49 +0600

    fast cout added

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #define GREEN   "\033[32m"      /* Green */
 //#define YELLOW  "\033[33m"      /* Yellow */
 //#define BLUE    "\033[34m"      /* Blue */
-//#define MAGENTA "\033[35m"      /* Magenta */
+#define MAGENTA "\033[35m"      /* Magenta */
 //#define CYAN    "\033[36m"      /* Cyan */
 //#define WHITE   "\033[37m"      /* White */
 //#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
@@ -131,6 +131,7 @@ public:
 
 int main() {
     ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     Screen screen;
     Menu menu;

f149122eaa539fe6048db6c96fb1859a47c3e470
Mrcoif <svzagarov@gmail.com>
2023-11-26 21:28:28 +0600

    починил таблицу

--- src/main.cpp
+++ src/main.cpp
@@ -255,7 +255,7 @@ protected:
         };
         for (int i = 0; i < N; i++) {
             menuItems.emplace_back("|        |        |            |           |");
-            sprintf(menuItems[i + 3].data(), "|   %-2d   | %#2g | %#8g | %#8g", i, XF1F2[0][i], XF1F2[1][i],
+            sprintf(menuItems[i + 3].data(), "|   %-2d   | %#7.5f | %#9g | %#9g |", i+1, XF1F2[0][i], XF1F2[1][i],
                     XF1F2[2][i]);
             menuItems[i + 3][21] = (XF1F2[1][i] == maxF1) ? Color::GREEN : (XF1F2[1][i] == minF1) ? Color::MAGENTA
                                                                                                   : ' ';
